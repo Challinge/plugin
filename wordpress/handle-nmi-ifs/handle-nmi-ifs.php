a8bea44fc0c40d8a72b5a24979bf1fb354ea71ce
Franck ADOUKO <fvadouko@live.fr>
2015-01-18 20:57:08 +0000

    handling nmi and ifs
    This plugin creates an amount and information for a customer to nmi.
    This plugin also creates a new contact to infusionsoft, and adds a
    commission to an affiliate recorded in the worpress database

--- wordpress/handle-nmi-ifs/handle-nmi-ifs.php
+++ wordpress/handle-nmi-ifs/handle-nmi-ifs.php
@@ -13,10 +13,10 @@
  */
 
 // First of all Need to activate the plugins : infusionsoft sdk, infusionsoft developpers plugin
-//add_action( 'admin_post_add_foobar', 'prefix_admin_add_foobar' );
-add_action( 'wp_footer', 'prefix_admin_add_foobar'); // for checking
+add_action( 'admin_post_add_foobar', 'prefix_admin_add_foobar' );
+//add_action( 'wp_footer', 'prefix_admin_add_foobar'); // for checking
 function prefix_admin_add_foobar() {
-       status_header(200);
+    status_header(200);
 
     //******************** HANDLE NMI ********************//
     $gw = new gwapi;
@@ -29,208 +29,71 @@ function prefix_admin_add_foobar() {
     $gw->setOrder("1234", "Big Order", 1, 2, "PO1234", $_SERVER['REMOTE_ADDR']);
 
     $r = $gw->doSale("0.2", "4111111111111111", "1025", "999");
+    print $gw->responses['responsetext'];
 
-    print $gw->responses['responsetext']. '<br />';
+    //******************** REGISTER USER TO INFUSIONSOFT ********************//
+    // FROM <form action="https://loweryourtuition.org/landing-page/wp-admin/admin-post.php" method="post">
+    // Load the Infusionsoft API class
+    require_once('duisdk/src/isdk.php');
 
-    // if payment is not successful, there is no meaning of adding data in the IFS
-    if ( $gw->responses['responsetext'] == 'DECLINE') {
-        print 'Payment succeded in NMI' . '<br />';
+    $ifs = new iSDK();
 
+    $ifs->cfgCon("xp180", "219753c29b8ac0b7ed594fb97fa447fe");
 
-        //******************** REGISTER USER TO INFUSIONSOFT ********************//
-        // FROM <form action="https://loweryourtuition.org/landing-page/wp-admin/admin-post.php" method="post">
-        // Load the Infusionsoft API class
-        require_once('duisdk/src/isdk.php');
+    $conDat = array('FirstName' => $_REQUEST['firstname'],
+        'LastName'  => $_REQUEST['lastname'],
+        'Email'     => $_REQUEST['emailaddress']);
 
-        $ifs = new iSDK();
+    $conID = $ifs->addCon($conDat);
 
-        $ifs->cfgCon("xp180", "219753c29b8ac0b7ed594fb97fa447fe");
+    print 'Contact added : '.$conID. '<br />';
 
-        $conDat = array('FirstName' => $_REQUEST['firstname'],
-            'LastName' => $_REQUEST['lastname'],
-            'Email' => $_REQUEST['emailaddress']);
+    // if you want to add another method like a campaign do it as above : $contact = $ifs->campaign(your mehod, $data );
 
-       $conID = $ifs->addCon($conDat);
+    //******************** CREATE A COMMISSION FOR AN AFFILIATE TO INFUSIONSOFT ********************//
+    // Create an invoice
+    $currentDate = date("d-m-Y");
+    $oDate = $ifs->infuDate($currentDate);
+    $invoiceId = $ifs->blankOrder("219753c29b8ac0b7ed594fb97fa447fe","Payment of a student", $oDate, 0, 0);
 
-        print 'Contact added : ' . $conID . '<br />';
+    //echo 'The id invoice : '.$invoiceId . '<br />'; // for checking
 
-        // if you want to add another method like a campaign do it as above : $contact = $ifs->campaign(your mehod, $data );
+    // Create a payment : this one exits because it's useful if you accept cash/check, or handle payments outside of Infusionsoft.
+    $pDate = $ifs->infuDate($currentDate);
+    $operation = $ifs->manualPmt($invoiceId,9.95,$pDate,$_REQUEST['cardtype'],'$'. $invoiceId .' paid by '. $_REQUEST['cardtype'],false);
+    if ($operation) {
+        print 'Manual Payment Successful'. '<br />';
+    } else {
+        print 'Manual Payment Failed'. '<br />';
+    }
 
-        //******************** CREATE A COMMISSION FOR AN AFFILIATE TO INFUSIONSOFT ********************//
-        /*
-        $programs = $ifs->etResourcesForAffiliateProgram(24);
-        foreach( $programs as $cle => $element)
-        {
-            foreach ( $element as $key => $value){
-                echo '[' . $key . '] vaut ' . $value . '<br />';
-            }
+    // Creates a commission on an existing invoice
+     $pageID = get_the_ID();
+    global $wpdb;
+    $affiliateId='';
+    $anAffiliate = $wpdb->get_row("SELECT idaffiliate, productid, amount, percentage FROM wpifsnmmi_handleaffiliates WHERE idpage=".$pageID, ARRAY_A);
 
-        }
+    if ( $anAffiliate ) {
+        $affiliateId = $anAffiliate['idaffiliate'];
+        $productId = $anAffiliate['productid'];
+        $amount = $anAffiliate['amount'];
+        $percentage = $anAffiliate['percentage'];
+
+        /* For Checking
+         * echo 'id affiliate :'.$affiliateId. '<br />';
+        echo 'id product :'.$productId. '<br />';
+        echo 'id amount :'.$amount. '<br />';
+        echo 'percentage :'.$percentage. '<br />';
         */
-        /*
-        echo '<pre>';
-        print_r($programs);
-        echo '</pre>';*/
+    }
 
-        /*
-        $result = $ifs->getResourcesForAffiliateProgram(6);
-
-        echo '<pre>';
-        print_r($result);
-        echo '</pre>';
-        */
-        /*
-        $returnFields = array('Id','NotifySale', 'NotifyLead','PayoutType');
-        $query = array('NotifySale' => '1');
-        $affiliate = $ifs->dsQuery("Affiliate",10,0,$query,$returnFields);
-        */
-        /*
-        $returnFields = array('Id','PlanPrice', 'ProductId');
-        $affiliate = $ifs->dsFind('SubscriptionPlan',5,0,'Id',2,$returnFields);
-        */
-
-        /* get id of the product
-        $returnFields = array('Id','ProductPrice', 'ProductName');
-        $query = array('ProductName' => 'Annual Membership LYT');
-        $affiliate = $ifs->dsQuery("Product",10,0,$query,$returnFields);
-        */
-
-        /*
-        $affiliateIds = 24;
-        $start = date('Ymd\TH:i:s',mktime(00,00,00,9,01,2013));
-        $finish = date('Ymd\TH:i:s',mktime(00,00,00,1,01,2015));
-        $affiliate = $ifs->affPayouts($affiliateIds,$start,$finish);
-        */
-
-        /* get all commission for a specific affiliate
-        $affiliateIds = 24;
-        $start = date('Ymd\TH:i:s',mktime(00,00,00,9,01,2013));
-        $finish = date('Ymd\TH:i:s',mktime(00,00,00,1,01,2015));
-        $affiliate = $ifs->affCommissions($affiliateIds,$start,$finish);*/
-
-        // to get all invoices for a specific affiliate
-    /*
-
-         $returnFields = array('Id','DateCreated', 'Description', 'Synced','ProductSold', 'TotalPaid', 'TotalDue', 'LeadAffiliateId');
-        $query = array('AffiliateId' => '24');
-        $affiliate = $ifs->dsQuery("Invoice",200,0,$query,$returnFields);
-
-        echo '<pre>';
-        print_r($affiliate);
-        echo '</pre>';
-
-        /*
-        $affiliate = $ifs->getPayments(300);
-
-        echo '<pre>';
-        print_r($affiliate);
-        echo '</pre>';
-*/
-        /*
-        $returnFields = array('Id','Amt', 'InvoiceId', 'PayDate','PayStatus', 'PaymentId');
-        $query = array('InvoiceId' => '300');
-        $affiliate = $ifs->dsQuery("InvoicePayment",200,0,$query,$returnFields);
-        echo '<pre>';
-        print_r($affiliate);
-        echo '</pre>';
-        /*
-         for($i=478; $i<=546; $i++){
-             $ifs->deleteInvoice($i);
-         }*/
-
-        /*
-        $affiliate = $ifs->getPayments(300);
-
-        echo '<pre>';
-        print_r($affiliate);
-        echo '</pre>';
-
-        $affiliate = $ifs->getPayments(556);
-
-        echo '<pre>';
-        print_r($affiliate);
-        echo '</pre>';
-*/
-        // Search an affiliateId from pageId
-        $pageID = get_the_ID();
-        global $wpdb;
-        $affiliateId = '';
-        $anAffiliate = $wpdb->get_row("SELECT idaffiliate, tagone, tagtwo FROM wpifsnmmi_handleaffiliates WHERE idpage=" . $pageID, ARRAY_A);
-
-        if ($anAffiliate) {
-            $affiliateId = $anAffiliate['idaffiliate'];
-            $tagone = $anAffiliate['tagone'];
-            $tagtwo = $anAffiliate['tagtwo'];
-
-            /* For Checking
-             * echo 'id affiliate :'.$affiliateId. '<br />';
-            echo 'id product :'.$productId. '<br />';
-            echo 'id amount :'.$amount. '<br />';
-            echo 'percentage :'.$percentage. '<br />';
-            */
-        }
-
-        // Create a blank order :  a one-time order with no added line items
-        $currentDate = date("d-m-Y");
-        $oDate = $ifs->infuDate($currentDate);
-        $invoiceId = $ifs->blankOrder($conID, "Payment of a student", $oDate, $anAffiliate, $anAffiliate);
-
-        echo 'The id invoice : '.$invoiceId . '<br />'; // for checking
-
-        // create a line in this blank order
-        $result = $ifs->addOrderItem($invoiceId, 20, 6, 155.4, 1, "Turbo Booster", "Jay's Awesome Turbo Booster");
-
-        echo 'The line order was created : '.$result . '<br />'; // for checking
-
-        // Create a payment : this one exits because it's useful if you accept cash/check, or handle payments outside of Infusionsoft.
-        $pDate = $ifs->infuDate($currentDate);
-        $operation = $ifs->manualPmt($invoiceId, 155.4, $pDate, 'Credit Card','$12.95 paid by Credit Card', true);
-        if ($operation) {
-            print 'Manual Payment Successful' . '<br />';
-        } else {
-            print 'Manual Payment Failed' . '<br />';
-        }
-
-        // Creates a commission on an existing invoice
-
-
-        $cDate = $ifs->infuDate($currentDate);
-        // Change the id affiliate, the id product the percentage the commission and the percentage from option page
-        $operation = $ifs->commOverride($invoiceId, $anAffiliate, 20, 22, 35.4, 5, "No Description", $cDate);
-        if ($operation) {
-            print 'Commission being added Successful' . '<br />';
-        } else {
-            print 'Commission Failed' . '<br />';
-        }
-
-        $affiliateIds = 24;
-        $start = date('Ymd\TH:i:s',mktime(00,00,00,9,12,2014));
-        $finish = date('Ymd\TH:i:s',mktime(14,00,00,6,01,2015));
-        $affiliate = $ifs->affCommissions($affiliateIds,$start,$finish);
-
-        echo '<pre>';
-        print_r($affiliate);
-        echo '</pre>';
-
-            /*
-        $carray = array(
-            php_xmlrpc_encode($ifs->key),
-            php_xmlrpc_encode(554),
-            php_xmlrpc_encode(1),
-            php_xmlrpc_encode(1),
-            php_xmlrpc_encode(array(20, 4)),
-            php_xmlrpc_encode(array(20, 4)),
-            php_xmlrpc_encode(1),
-            php_xmlrpc_encode(array(10, 1)) // array of strings
-        );
-        $operation = $ifs->methodCaller("OrderService.placeOrder", $carray);
-
-        //$operation = $ifs->placeOrder(554,0,0,array(20,4),array(20,4),1,array("mmmm","mmmmmm"),24,24);
-
-        echo '<pre>';
-        print_r($operation);
-        echo '</pre>';
-            */
+    $cDate = $ifs->infuDate($currentDate);
+    // Change the id affiliate, the id product  the commission and the percentage from option page
+    $operation = $ifs->commOverride($invoiceId,$affiliateId,$productId,$percentage,$amount,5,"No Description",$cDate);
+    if ($operation) {
+        print 'Commission being added Successful'. '<br />';
+    } else {
+        print 'Commission Failed'. '<br />';
     }
 
 }
